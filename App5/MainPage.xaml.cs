92338b9df6516b1d6e4637da8b442886348e1142
John Eddy <jaydeflix@gmail.com>
2016-12-09 10:43:28 -0800

    Add project files.

---
+++ App5/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Popups;
+
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace App5
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        private async void button_Click(object sender, RoutedEventArgs e)
+        {
+            int i = textBoxHandleList.Text.Split('\n').Length;
+            int numLines = textBoxHandleList.Text.Length - textBoxHandleList.Text.Replace(Environment.NewLine, string.Empty).Length;
+            var dialog = new MessageDialog(i.ToString() + "-" + numLines.ToString());
+            await dialog.ShowAsync();
+        }
+    }
+}
